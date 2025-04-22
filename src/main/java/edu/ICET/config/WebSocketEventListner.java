488fc4cd30d2ae295e8b0fcb21fbb76e36664063
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-04-22 21:47:40 +0530

    create webSocketConfigeration

---
+++ src/main/java/edu/ICET/config/WebSocketEventListner.java
@@ -0,0 +1,24 @@
+package edu.ICET.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListner {
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListner(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+    }
+}
